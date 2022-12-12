15ab5863e3c439c7b89c507a6c4ac2cec438f4d8
dogubo mamuzo joshua <dogubo.joshua@decagon.dev>
2022-12-12 21:40:07 +0100

    prod updated

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -4,10 +4,10 @@ export default {
   APP_SECRETE: "",
   JWT_SECRETE: "",
   STAGE: "production",
-  DATABASE_USERNAME :'postgres',
-  DATABASE_HOST : 'localhost',
-DATABASE_DATABASE_NAME:'postgres',
-DATABASE_PASSWORD :'1234',
-DATABASE_PORT :5432,
+  DATABASE_USERNAME: "postgres",
+  DATABASE_HOST: "localhost",
+  DATABASE_DATABASE_NAME: "postgres",
+  DATABASE_PASSWORD: "1234",
+  DATABASE_PORT: 5432,
 };
 console.log("running production mode");
