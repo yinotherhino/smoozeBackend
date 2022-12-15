dea98f8efd74595ceb24f1634fc0cb3ad24b4a01
hakynbayo <akinbayo.akinwande@decagon.dev>
2022-12-15 03:29:13 +0100

    fgt/rst password

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { Register, signin, update } from "../handlers/userHandler";
+import { Register, signin, update, } from "../handlers/userHandler";
 import { auth } from "../middleware/auth/auth";
 import { loginUser, RegisterUser, updateUser } from "../utils/joi-validator";
+import { forgotPassword } from "../handlers/userHandler/userHandler";
+import { resetPassword } from "../handlers/userHandler/forgotpasswordhandler";
 
 export const usersRoute = express.Router();
 /**
@@ -17,3 +19,8 @@ export const usersRoute = express.Router();
 usersRoute.post("/signup", RegisterUser, Register);
 usersRoute.post("/signin",loginUser , signin);
 usersRoute.patch("/update",updateUser, auth,  update);
+usersRoute.post("/forgot-password", forgotPassword);
+usersRoute.post("/reset-password", resetPassword);
+
+
+
