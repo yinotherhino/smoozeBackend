bcda2212ae5e4024ac2b7b95fbd274930405aaf4
Decagon <theodorachinonso@gmail.com>
2022-12-15 09:47:56 +0100

    mail services

--- src/config/local.ts
+++ src/config/local.ts
@@ -6,7 +6,7 @@ export default {
   DATABASE_USERNAME: "postgres",
   DATABASE_HOST: "localhost",
   DATABASE_DATABASE_NAME: "postgres",
-  DATABASE_PASSWORD: "12567",
+  DATABASE_PASSWORD: "1234",
   DATABASE_PORT: 5432,
 };
 console.log("running local mode");
