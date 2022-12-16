e845cde852d9881689cec3b4a2248e94ff1d9dd1
dogubo mamuzo joshua <dogubo.joshua@decagon.dev>
2022-12-16 21:24:32 +0100

    update env

--- .envfile.ts
+++ .envfile.ts
@@ -1,8 +1,24 @@
-/*
-#please uncomment this multiline commnent and rename this file to .env then delete  
-DATABASE_URL=""
-APP_SECRETE=""
-JWT_SECRETE=""
-STAGE=""
-NODE_ENV=""
-*/
+// #please uncomment this multiline commnent and rename this file to .env then delete
+// # DATABASE_URL=""
+// # APP_SECRETE=""
+// # JWT_SECRETE=""
+// # STAGE=""
+// # NODE_ENV=""
+// GMAIL_PASS="lxideqvkiznxipjs"
+
+// GMAIL_USER="smooveappro@gmail.com"
+
+// FROM_ADMIN_EMAIL="smooveappro@gmail.com"
+
+// userSubject =" Welcome To Smooze App!"
+// DATABASE_LOCALPASSWORD="12567"
+// BASE_URL="http:localhost:7000"
+//   DATABASE_URL= "postgres://rtajhzcb:vEUxVGeppmcvGoMPCxYsryRU76SGLzy4@ruby.db.elephantsql.com/rtajhzcb",
+//   APP_SECRETE="blablabla",
+//   JWT_SECRETE="blablabla",
+//   DATABASE_USERNAME="rtajhzcb",
+//   DATABASE_HOST= "ruby.db.elephantsql.com",
+//   DATABASE_DATABASE_NAME="rtajhzcb",
+//   DATABASE_PASSWORD="vEUxVGeppmcvGoMPCxYsryRU76SGLzy4",
+//   BASE_URL="http://localhost:7000"
+//   FRONTEND_BASE_URL="http://localhost:5173"
