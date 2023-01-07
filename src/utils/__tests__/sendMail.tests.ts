b1b8055eca62f089f75133f778fe001fe9cc0bac
yinotherhino <muhammedbayero@gmail.com>
2023-01-07 09:16:58 +0100

    mail service tests error case

--- src/utils/__tests__/sendMail.tests.ts
+++ src/utils/__tests__/sendMail.tests.ts
@@ -1,11 +1,46 @@
-import { sendEmail } from "../notification"
-
-test("",()=>{expect(1+2).toBe(3)})
+import { sendEmail as Mailer } from "../notification"
 
 describe("Email service test", ()=>{
-    test("Should send an email successfully", async() =>{
-        const sendMail = await sendEmail('yinorhino@gmail.com',"Email test passed","<h1>Email test passed</h1>")
-        console.log(sendMail?.accepted)
-        expect(sendMail).toBeFalsy()
+
+    //commented out to save email credits
+    // test("Should send an email successfully", async() =>{
+    //     const sendMail = await Mailer('yinorhino@gmail.com',"Email test passed","<h1>Email test passed</h1>")
+    //     const sent = sendMail?.response
+    //     expect(sent).toContain("2.0.0 OK")
+    // })
+
+    test("Should throw error when subject", async() =>{
+    expect.assertions(1);
+      try {
+        await Mailer('yinorhino@gmail.com',"","<h1>Email test passed</h1>")
+
+    } catch (err:any) {
+        const errorMessage = err?.message
+        expect(errorMessage).toBe("subject not specified");
+    }
+    })
+
+    test("Should throw error when html is not passed as argument", async() =>{
+        expect.assertions(1);
+        try {
+            await Mailer('yinorhino@gmail.com',"Email test passed","")
+
+        } catch (err:any) {
+            const errorMessage = err?.message
+            console.log(err.message)
+            expect(errorMessage).toBe("html template not specified");
+        }
+    })
+
+    test("Should throw error when to is not passed as argument", async() =>{
+        expect.assertions(1);
+        try {
+            await Mailer("","Email test passed","<h1>Email test passed</h1>")
+
+         } catch (err:any) {
+             const errorMessage = err?.message
+             console.log(err.message)
+             expect(errorMessage).toBe("recipient(to) not specified");
+         }
     })
 })
--- src/utils/notification/sendMail.ts
+++ src/utils/notification/sendMail.ts
@@ -14,6 +14,10 @@ const transport = nodemailer.createTransport({
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
   try {
+    if(to.length<5 || subject.length<1 || html.length<1){
+      const errorMessage = to.length<5 ? "recipient(to) not specified" : subject.length<1  ? "subject not specified" : "html template not specified"
+      throw new Error(errorMessage)
+    }
     const response = await transport.sendMail({
       from: config.FROM_ADMIN_EMAIL,
       to,
@@ -22,6 +26,6 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
     });
     return response;
   } catch (error) {
-    console.log(error);
+    throw(error);
   }
 };
