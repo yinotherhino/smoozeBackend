9384715e3941aa63db5c93e08dab379c6e9f52bd
yinotherhino <muhammedbayero@gmail.com>
2023-01-07 06:44:17 +0100

    tests for config

--- src/config/db.ts
+++ src/config/db.ts
@@ -12,3 +12,4 @@ export const db = new Sequelize(
     logging: false,
   }
 );
+console.log(db)
--- src/handlers/__tests__/handler.test.ts
+++
@@ -1,5 +0,0 @@
-describe("stating", () => {
-  it("should always pass", () => {
-    expect("smoove").toBe("smoove");
-  });
-});
