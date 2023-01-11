9b2b03471568b91b7ac974dcfb881477dee7e911
chokovel <sweetelf20@gmail.com>
2023-01-11 22:50:14 +0100

    server security

--- src/utils/multer/multer.ts
+++ src/utils/multer/multer.ts
@@ -3,14 +3,12 @@ import multer from "multer";
 const cloudinary = require("cloudinary").v2;
 
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-// import config from "../../config";
-import dotenv from "dotenv"
-dotenv.config()
+import config from "../../config";
 
 cloudinary.config({
-  cloud_name: 'ddtii3waj', 
-  api_key: '976457571935162', 
-  api_secret: 'dRzFjskey3MI07FaQgN7VtQUjkw' 
+  cloud_name: config.CLOUD_NAME,
+  api_key: config.CLOUD_API_KEY,
+  api_secret: config.CLOUD_API_SECRET,
 });
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
